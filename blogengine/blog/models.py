5e145587af855195dd2e56a5db1bc803b98e138b
kani0798 <arstanbaevakani@gmail.com>
2020-08-05 18:48:01 +0600

    Created project blogengine, created models, templates.

---
+++ blogengine/blog/models.py
@@ -0,0 +1,20 @@
+
+from django.db import models
+
+# Create your models here.
+from django.urls import reverse
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150, db_index=True)
+    slug = models.SlugField(max_length=150, unique=True)
+    body = models.TextField(blank=True, db_index=True)
+    date_pub = models.DateTimeField(auto_now_add=True)
+
+    def get_absolute_url(self):
+        return reverse('post_detail_url', kwargs={'slug': self.slug})
+
+
+    def __str__(self):
+        return f"{self.title}"
+
